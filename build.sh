a7471b5147d8bef8d4916e537a0bce8a124b1344
UFifty50 <UFifty50.github@gmail.com>
2021-11-27 13:53:43 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,42 +1,44 @@
+#!/bin/sh
+
 # THIS IS ALL BATCH AT THE MOMENT, DO NOT RUN
 # IT WONT WORK, TRUST ME
 
-which /q python3.9
-IF ERRORLEVEL 1 (
-    ECHO Python is missing. Ensure it is installed and placed in your PATH.
-    EXIT /B
-) ELSE (
-    for /F "tokens=*" %%A in ('where python') do set python=%AA
-    for /F "tokens=*" %%A in ('%python% --version') do set pyver=%%A
-    for /F "tokens=*" %%A in ("%pyver%") do (
-        set "pyver=%%A"
-    )
-    set py=%pyver:~0,1%
-    IF NOT py == 3 (
-        ECHO Your python version is too old! Python 3.9 is required.
-        EXIT /B
-    ) ELSE (
-        ECHO Python found. Let's go!
-    )
-)
+which python &> /dev/null
+if [ $? = 1 ]; then
+    echo "Python is missing. Ensure it is installed and placed in your PATH."
+    exit
+else
+    python="$(which python)"
+    pyver="$($python -c "import sys; print(sys.version_info[0:2])")"
+    required="(3, 9)"
+    if $pyver != "$required"; then
+        echo "Your python version is too old! Python 3.9 is required."
+        exit
+    else
+        echo "Python found. Let's go!"
+    fi
+fi
 
-set input=ming.py
-set debugargs=--windows-disable-console --windows-force-stderr-spec=err.txt 
-set verboseargs=--show-scons --plugin-enable=pylint-warnings --show-progress --plugin-enable=pylint-warnings 
-set speedargs=--enable-plugin=anti-bloat --noinclude-pytest-mode=nofollow --noinclude-setuptools-mode=nofollow --python-flag=nosite 
-set basecmd=%python% -m nuitka --include-data-file=dlls\libfluidsynth.dll=mingus\midi\libfluidsynth.dll --include-data-file=local_pygame_menu\resources\fonts\*.ttf=pygame_menu\resources\fonts\ --include-data-file=dlls\libfluidsynth.dll=libfluidsynth.dll  --include-data-file=soundfonts\*.sf2=soundfonts\ --include-data-file=imgs\*=imgs\ --include-data-file=dlls\*.dll=dlls\ --windows-icon-from-ico=favicon.ico --include-package-data pygame_menu --plugin-enable=numpy  --onefile --onefile-windows-splash-screen-image=D:\Jonathan\Documents\ming\cornflower.png 
+input="ming.py"
+debugargs="--windows-disable-console --windows-force-stderr-spec=err.txt"
+verboseargs="--show-scons --plugin-enable=pylint-warnings --show-progress --plugin-enable=pylint-warnings"
+speedargs="--enable-plugin=anti-bloat --noinclude-pytest-mode=nofollow --noinclude-setuptools-mode=nofollow --python-flag=nosite"
+basecmd="$python -m nuitka --include-data-file=dlls/libfluidsynth.dll=mingus/midi/libfluidsynth.dll --include-data-file=local_pygame_menu/resources/fonts/*.ttf=pygame_menu/resources/fonts/ --include-data-file=dlls/libfluidsynth.dll=libfluidsynth.dll  --include-data-file=soundfonts/*.sf2=soundfonts/ --include-data-file=imgs/*=imgs/ --include-data-file=dlls/*.dll=dlls/ --windows-icon-from-ico=favicon.ico --include-package-data pygame_menu --plugin-enable=numpy  --onefile --onefile-windows-splash-screen-image=D:/Jonathan/Documents/ming/cornflower.png"
 
 
-set /p output="What should the exe be named? "
-set /p debug="Compile with debug options? (y/n) "
-set /p verbose="Compile verbosely? (y/n) "
-set /p speed="Compile with optimizations? (y/n, default is y) "
+read -p "What should the exe be named? " output
+read -p "Compile with debug options? (y/n) " debug
+read -p "Compile verbosely? (y/n) " verbose
+read -p "Compile with optimizations? (y/n, default is y) " speed
 
-if %speed% == y set basecmd=%basecmd%%speedargs%
-if %debug% == y set basecmd=%basecmd%%debugargs%
-if %verbose% == y set basecmd=%basecmd%%verboseargs%
-if %NUMBER_OF_PROCESSORS% == 1 set cpus=1
-if %NUMBER_OF_PROCESSORS% == 2 set cpus=1
-if not %NUMBER_OF_PROCESSORS% == 1 set /A cpus=%NUMBER_OF_PROCESSORS%-2
+if [ $speed = y ]; then; basecmd="$basecmd $speedargs"; fi
+if [ $debug = y ]; then; basecmd="$basecmd $debugargs"; fi
+if [ $verbose = y ]; then; basecmd="$basecmd $verboseargs"; fi
+if [ `getconf _NPROCESSORS_ONLN` = 1 ]; then
+    cpus=1
+else if [ `getconf _NPROCESSORS_ONLN` = 2 ]; then
+   cpus=1
+else
+    cpus=$((`getconf _NPROCESSORS_ONLN` - 2))
 
-%basecmd% --output-dir=build -j %cpus%  -o build\%output%.exe %input%
+exec $basecmd --output-dir=build -j $cpus  -o build/"$output" $input
